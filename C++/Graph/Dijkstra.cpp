407ebc1e49f3303c4df0b38a67bcd4220fd94d78
Lafolia <violgdae@gmail.com>
2020-01-21 23:57:55 +0900

    Dijkstra の更新

--- C++/Graph/Dijkstra.cpp
+++ C++/Graph/Dijkstra.cpp
@@ -1,11 +1,13 @@
 struct data {
 	llong place;
 	llong cost;
+	llong before;
 
 	data() {};
-	data(llong place, llong cost) :
+	data(llong place, llong cost, llong before) :
 		place(place),
-		cost(cost)
+		cost(cost),
+		before(before)
 	{}
 
 	bool operator<(const data &ri) const {
@@ -23,17 +25,18 @@ vector<vector<data> > edges;
 
 vector<data> dijkstra(int start) {
 	priority_queue<data, vector<data>, greater<data> > que;
-	vector<data> dis(edges.size(), data(-1, inf));
-	dis[start] = data(start, 0);
+	vector<data> dis(edges.size(), data(-1, inf<llong>, -1));
+	dis[start] = data(start, 0, -1);
 
 	que.push(dis[start]);
 	while (que.size()) {
 		data now = que.top();
 		que.pop();
 		if (dis[now.place] < now) continue;
+		now = dis[now.place];
 
 		for (auto &edge: edges[now.place]) {
-			data next(edge.place, now.cost + edge.cost);
+			data next(edge.place, now.cost + edge.cost, now.place);
 			if(dis[next.place] <= next) continue;
 			dis[next.place] = next;
 			que.push(next);
